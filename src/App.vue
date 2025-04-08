4b6d34011bcdd16d72010ae9dbfc3328c02c10c3
Alexander Kostin <iheartapplejr@gmail.com>
2025-04-08 18:14:17 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<div>
+		<p>Text</p>
+	</div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [vue()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+					@use "sass:color";
+					@use "@/assets/scss/variables.scss" as *;
+				`
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		}
+	}
+})
